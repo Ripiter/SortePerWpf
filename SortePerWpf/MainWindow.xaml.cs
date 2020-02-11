427709e63ef141378e4b749c542055c85c7426f5
Peter <ripiter15@wp.pl>
2020-02-11 11:51:27 +0100

    Add project files.

---
+++ SortePerWpf/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace SortePerWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            steve.ItemsSource = people;
+            this.DataContext = this;
+        }
+
+        ObservableCollection<Person> people = new ObservableCollection<Person>
+        {
+            new Person("Jon", "15"),
+            new Person("Jack", "9"),
+            new Person("Stefan", "20")
+        };
+
+        Person currectPlayer;
+        public void RemovePerson()
+        {
+            people.RemoveAt(0);
+        }
+
+        
+        int nextPlayer = 1;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Set next player in line
+            if (nextPlayer >= people.Count)
+                nextPlayer = 0;
+
+            
+            MyTextBlock.Dispatcher.BeginInvoke(new Action(() => MyTextBlock.DataContext = currectPlayer));
+            nextPlayer++;
+        }
+
+    }
+}
---
+++ SortePerWpf/Player.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortePerWpf
+{
+    abstract class Player
+    {
+        List<Card> playersCard = new List<Card>();
+        string name;
+
+        public List<Card> PlayersCards
+        {
+            get
+            {
+                return this.playersCard;
+            }
+            set
+            {
+                this.playersCard = value;
+            }
+        }
+        public string Name { get => name; set => name = value; }
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// Takes card from another player
+        /// </summary>
+        public virtual string TakeCard(Player player, int cardPlace)
+        {
+            Card card = player.PlayersCards[cardPlace];
+
+            player.PlayersCards.Remove(card);
+            int rndPos = new Random(DateTime.Now.Millisecond).Next(0, PlayersCards.Count);
+            this.PlayersCards.Insert(rndPos, card);
+
+            return RemovePairs(card);
+        }
+
+
+        public string RemovePairs(Card cardFromPlayer)
+        {
+            string temp = string.Empty;
+            foreach (Card card in PlayersCards.ToList())
+            {
+                if (card.Number == cardFromPlayer.Number && card.Type != cardFromPlayer.Type)
+                {
+                    temp += "Match " + card.Number + " " + card.Type + "\n" +
+                            "with " + cardFromPlayer.Number + " " + cardFromPlayer.Type;
+
+                    PlayersCards.Remove(card);
+                    PlayersCards.Remove(cardFromPlayer);
+                }
+            }
+            return temp;
+        }
+
+
+        public virtual string RemoveAllPairs()
+        {
+            string temp = string.Empty;
+            foreach (Card nCard in PlayersCards.ToList())
+            {
+                foreach (Card card in PlayersCards.ToList())
+                {
+                    if (card.Number == nCard.Number && card.Type != nCard.Type)
+                    {
+                        temp += "Card from " + this.Name + " removed " + card.Number + " " + card.Type + "\n" +
+                                "Card from " + this.Name + " removed " + nCard.Number + " " + nCard.Type + "\n\n";
+
+                        //removes pair
+                        PlayersCards.Remove(nCard);
+                        PlayersCards.Remove(card);
+                    }
+                }
+            }
+            return temp;
+        }
+    }
+}
